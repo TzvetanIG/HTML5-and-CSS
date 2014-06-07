cbfc2f39915cae13370200117c2d2b9e0725ca85
TzvetanIG <cecoig@mail.bg>
2014-06-07 11:06:20 +0300

    Add calculator

---
+++ HTML5-Tables-Homework/js/calculator.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Ganchev on 7.6.2014 г..
+ */
+
+function Calculator(screenId) {
+    this.expresion = "0";
+    this.calculatorScreen = document.getElementById(screenId);
+    this.calculatorScreen.value = this.expresion;
+}
+
+Calculator.prototype.addChar = function(symbol){
+    if (this.expresion == "0"){
+        this.expresion = "";
+    }
+    this.expresion += symbol;
+    this.calculatorScreen.value = this.expresion;
+}
+
+Calculator.prototype.printResult = function(){
+    this.calculatorScreen.value = eval(this.expresion);
+    this.expresion = "0";
+}
+
+Calculator.prototype.clearScr = function(){
+    this.expresion = "0";
+    this.calculatorScreen.value = this.expresion;
+
+}
+
+calculator = new Calculator("screen");
